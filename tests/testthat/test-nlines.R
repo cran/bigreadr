cac86a266e58d84151f3bfa758b6c8b9efeb8dd0
Florian Privé <florian.prive.21@gmail.com>
2019-09-07 15:20:06 +0000

    version 0.1.9

--- tests/testthat/test-nlines.R
+++ tests/testthat/test-nlines.R
@@ -4,15 +4,26 @@ context("test-nlines.R")
 
 ################################################################################
 
-test_that("'nlines' works", {
-  SEQ <- seq_len(nrow(iris))
+test_that("'nlines()' works", {
+
+  expect_error(nlines("does_not_exist.txt"))
+
   strings <- c("", "", " ", sapply(10^(seq(0, 4, by = 0.2)), function(i) {
-    paste(as.matrix(iris)[sample(SEQ, i, TRUE), ], collapse = " ")
+    paste(as.matrix(iris)[sample(nrow(iris), i, TRUE), ], collapse = " ")
   }))
-  replicate(10, {
+  replicate(100, {
     writeLines(sample(strings, replace = TRUE), tmp <- tempfile())
-    expect_identical(nlines(tmp), 24L)
+    expect_identical(nlines(tmp), 24)
   })
+})
+
+################################################################################
+
+test_that("'nlines()' works with or without newline", {
+  csv1 <- system.file("testdata", "cars_with_newline.csv", package = "bigreadr")
+  expect_identical(nlines(csv1), 51)
+  csv2 <- system.file("testdata", "cars_without_newline.csv", package = "bigreadr")
+  expect_identical(nlines(csv2), 51)
 })
 
 ################################################################################
